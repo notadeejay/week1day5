2eb541d98d5e0c90d912f6b06f34c789afa649e2
Amy <amy@lighthouselabs.ca>
2017-07-28 00:10:12 +0000

    Importing and exporting modules

---
+++ modulesexport.js
@@ -0,0 +1,7 @@
+var privateModule = function() {
+  return "(X+Y) is to X as X is to Y: (X+Y)≈" + 1.618;
+  }
+
+module.exports = {
+  explain: privateModule
+};
---
+++ modulesimport.js
@@ -0,0 +1,5 @@
+var coolMath = require("./modulesexport");
+
+ // log a Number
+
+console.log(coolMath.explain());
