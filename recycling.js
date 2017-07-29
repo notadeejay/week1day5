bb3bfcd62494e117f771a5611d4f833a324d003b
Amy <amy@lighthouselabs.ca>
2017-07-29 01:13:26 +0000

    Added a function that returns the number of bottles redeemed by caps and bottles

--- recycling.js
+++ recycling.js
@@ -1,10 +1,13 @@
 var moneySpent = process.argv.slice(2);
 
 function returnNumberOfBottles (moneySpent) {
-  var bottlesPurchased = moneySpent / 2
-  var maxBottles = (bottlesPurchased * 4 - 5)
+  var bottlesPurchased = moneySpent / 2;
+  var maxBottles = (bottlesPurchased * 4 - 5);
+  var fromCaps = Math.floor(maxBottles * .25);
+  var fromBottles = Math.floor(maxBottles * .50);
 
-  console.log("If you spend $" + moneySpent + " you will be able to redeem " + Math.floor(maxBottles) + " bottles");
+  console.log("If you spend $" + moneySpent + " you will be able to redeem " + Math.floor(maxBottles) +
+    " bottles. " + fromBottles + " bottles will be redeemed by bottles and " + fromCaps + " will be redeemed from caps.");
 }
 
 returnNumberOfBottles(moneySpent);
